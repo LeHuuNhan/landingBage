567dc33fa116ea5def3847f385e4f37d26fdcf02
Huu Nhan <lehuunhan529@gmail.com>
2021-08-26 15:03:07 +0700

    ban 1

---
+++ src/cpn/home.tsx
@@ -0,0 +1,234 @@
+import React from 'react';
+import '../App.scss';
+
+function Home() {
+
+  // constructor(props) {
+
+  //   super(props);
+
+  //   this.state = {
+  //     items: [
+  //       {
+  //         title:  "Promoting your territory towards foreign tourists",
+  //         content: "EasyCamper wants to promote the excellence of Italian food and wine, art, culture and the economy."
+  //       }
+  //     ]
+  //   }
+
+  // }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="log">
+          <select name="cars" id="cars">
+          {/* <img src="../img/XMLID_119.png" alt="" className="iconLog"/> */}
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+          </select>
+        </div>
+          <div className="popup">
+            <div className="title">
+               Find out how we can <span className ="highlight">save your time</span> in Backpacking in Europe
+            </div>
+            <div className="content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac amet id eget scelerisque amet etiam in sit commodo. Pretium ut duis orci pulvinar pretium id consectetur.</p>
+            </div>
+            <div className="explore">
+              <div className="icon_explore"></div>
+              <a className="btn_explore" href="!#" >
+                Explore
+              </a>
+            </div>
+          </div>
+      </header>
+
+      <body>
+          <div className="body1">
+            <div className="body1_1" >
+              <img src="../img/undraw_exploring_1l7f 1.png" alt="" className="hinh1"/>
+              <div className="body1_content1">
+                <h1>Promoting your territory towards foreign tourists</h1>
+                <p>EasyCamper wants to promote the excellence of Italian food and wine, art, culture and the economy.</p>
+                <b>Discover the places linked to your area that you can link to your structure</b>
+              </div>
+            </div>
+
+            <div className="body1_1">
+              <img src="../img/undraw_tourist_map_re_293e 1.png" alt="" className="hinh1"/>
+              <div className="body1_content1">
+                <h1>Promoting your territory towards foreign tourists</h1>
+                <p>EasyCamper wants to promote the excellence of Italian food and wine, art, culture and the economy.</p>
+                <b>Discover the places linked to your area that you can link to your structure</b>
+              </div>
+            </div>
+
+            <div className="body1_1">
+              <img src="../img/undraw_Destination_re_sr74 1.png" alt="" className="hinh1"/>
+              <div className="body1_content1">
+                <h1>Promoting your territory towards foreign tourists</h1>
+                <p>EasyCamper wants to promote the excellence of Italian food and wine, art, culture and the economy.</p>
+                <b>Discover the places linked to your area that you can link to your structure</b>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="body2">
+            <h1>Making your facility known is our priority</h1>
+
+            <div className="body2_content">
+              <div className="body2_content1">
+                <img src="../img/icon2.png" alt="" className="icon1"/>
+                <b className="b_choose">Choose the type of stop</b>
+                <p>Select the type of stopover you are ordering from Equipped Area, Camping and Agricamping</p>
+                <b className="b_show">Show me more</b>
+              </div>
+              
+              <div className="body2_content1">
+                <img src="../img/icon3.png" alt="" className="icon1"/>
+                <b className="b_choose">Choose the type of stop</b>
+                <p>Select the type of stopover you are ordering from Equipped Area, Camping and Agricamping</p>
+                <b className="b_show">Show me more</b>
+              </div>
+
+              <div className="body2_content1">
+                <img src="../img/icon1.png" alt="" className="icon1"/>
+                <b className="b_choose">Choose the type of stop</b>
+                <p>Select the type of stopover you are ordering from Equipped Area, Camping and Agricamping</p>
+                <b className="b_show">Show me more</b>
+              </div>
+
+              <div className="body2_content1">
+                <img src="../img/icon1.png" alt="" className="icon1"/>
+                <b className="b_choose">Choose the type of stop</b>
+                <p>Select the type of stopover you are ordering from Equipped Area, Camping and Agricamping</p>
+                <b className="b_show">Show me more</b>
+              </div>
+            </div>
+
+          </div>
+          <div className="body3">
+            <div className="body3_1">
+              <h1>Join the community campervan sites managers.</h1>
+              <p>Discover all the equibbed areas in Italy. Find the shop with all the services for you and your camper.</p>
+              <div className="start">
+                <div className="icon_start"></div>
+                <a className="btn_start" href="!#" >
+                  Start
+                </a>
+              </div>
+            </div>
+
+            <div className="body3_2">
+                <div className="body3_content">
+                  <img src="" alt="" className="sp1"/>
+                  <p className="title">Lucciano</p>
+                  <p className="describe">Rome</p>
+                  <p className="view">View the Site</p>
+                </div>
+
+                <div className="body3_content">
+                  <img src="" alt="" className="sp1"/>
+                  <p className="title">Lucciano</p>
+                  <p className="describe">Rome</p>
+                  <p className="view">View the Site</p>
+                </div>
+
+                <div className="body3_content">
+                  <img src="" alt="" className="sp1"/>
+                  <p className="title">Lucciano</p>
+                  <p className="describe">Rome</p>
+                  <p className="view">View the Site</p>
+                </div>
+            </div>
+          </div>
+          <div className="body4">
+            <h1>Our Advantages</h1>
+            <div className="body4_content">
+              <div className="body4_content1">
+                <img src="../img/XMLID.png" alt="" className="iconXMLID"/>
+                <b className="b_choose">Convenience and Transparency</b>
+                <p className="body4_describe">You don’t have to find out later or only after camenierk and trarsparent Easyamper
+                 ratains pliacas and beautias of your tariaary and halpin a fixed commission on each transaction of 5</p>
+              </div>
+
+              <div className="body4_content1">
+                <img src="../img/XMLID.png" alt="" className="iconXMLID"/>
+                <b className="b_choose">Convenience and Transparency</b>
+                <p className="body4_describe">You don’t have to find out later or only after camenierk and trarsparent Easyamper
+                 ratains pliacas and beautias of your tariaary and halpin a fixed commission on each transaction of 5</p>
+              </div>
+
+              <div className="body4_content1">
+                <img src="../img/XMLID.png" alt="" className="iconXMLID"/>
+                <b className="b_choose">Convenience and Transparency</b>
+                <p className="body4_describe">You don’t have to find out later or only after camenierk and trarsparent Easyamper
+                 ratains pliacas and beautias of your tariaary and halpin a fixed commission on each transaction of 5</p>
+              </div>
+
+            </div>
+          </div>
+      </body>
+
+      <footer>
+          <div className="footer1">
+            <div className="footer1_left">
+              <p className="easy">Easy Camper</p>
+              <h1 className="when">When Passion Meets Comfort.</h1>
+            </div>
+
+            <div className="footer1_right">
+              <div className="content_right">
+                <b>Discover the Network</b>
+                <div className="content">
+                  <p>Where to shop</p>
+                  <p>Tour Guided</p>
+                  <p>Our Tours</p>
+                  <p>About Us</p>
+                </div>
+              </div>
+
+              <div className="content_right">
+                <b>Become a Manager</b>
+                <div className="content">
+                  <p>Join your Pairing</p>
+                  <p>Offer your Service</p>
+                  <p>Are you a guide?</p>
+                </div>
+              </div>
+
+              <div className="content_right">
+                <b>Assistance</b>
+                <div className="content">
+                  <p>Support Center</p>
+                  <p>FAQs</p>
+                  <p>Cancallation Options</p>
+                  <p>Reliablity and Sequrity</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr/>
+          <div className="footer2">
+            <div className="footer2_left">
+              <p>EazyCamper. All rights reserved. </p>
+            </div>
+
+            <div className="footer2_right">
+              <p>About</p>
+              <p>Site Map</p>
+              <p>Terms</p>
+              <p>Privacy Policy</p>
+              <p>Teams</p>
+            </div>
+          </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
